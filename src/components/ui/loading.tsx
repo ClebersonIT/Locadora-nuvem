4aafdc376b02ef2810c3ac37cbd902f7bee64a50
lucas.bittencourt <lucas.bittencourt@sspds.ce.gov.br>
2025-03-28 13:55:19 -0300

    remove unless components

--- src/components/ui/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { SentimentVeryDissatisfied } from "@mui/icons-material";
-
-export const Loading = () => {
-  return (
-    <div className=" rounded-md text-center h-max w-full p-4">
-      <h1 className="animate-pulse">Carregando...</h1>
-    </div>
-  );
-};
